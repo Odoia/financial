bd13a15f264d1d8df304db54202897a3a81550af
Tiago <tiagohenriqueodoia@gmail.com>
2022-03-23 11:32:12 -0300

    feature/models-creation Add a user model and tests
    - add validates too
    - after ness make relations db
    
    Signed-off-by: Tiago <tiagohenriqueodoia@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
 
+
+  validates :email, :password, :name, :surname, presence: true
+  validates :email, uniqueness: true
+  validates :password, length: { is: 8 }
+  validates :password, format: { with: /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])(?:([0-9a-zA-Z$*&@#])(?!\1)){8,}/,
+                                 message: 'password must has a least 1 capital letter, 1 number and 1 special character.' }
 end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user, class: '::User' do
     email { Faker::Internet.email }
     password { 'Test@123' }
-    active { true }
+    is_active { true }
     name { Faker::Name.first_name }
     surname { "#{Faker::Name.first_name} #{Faker::Name.last_name}" }
   end
