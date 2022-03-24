159677b9a1266b00bf23bfc879cbdc4441f2e110
Tiago <tiagohenriqueodoia@gmail.com>
2022-03-24 18:10:23 -0300

    feature/bank-account-index Add a show all bank account by user
    - fix a create user
    - befora this you can use create other objects no unalthenticated
    
    Signed-off-by: Tiago <tiagohenriqueodoia@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   post 'api/v1/authenticate', to: 'authentication#authenticate'
 
   post 'api/v1/user', to: 'user#create'
+  get 'api/v1/user/:id/bank_account', to: 'bank_account#show_all_by_user'
   post 'api/v1/bank_account', to: 'bank_account#create'
 end
